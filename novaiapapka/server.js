147a0f8a967776af2baad75da04a374d0ab28d99
G-oo-se <karimov.sultan.09123@gmail.com>
2025-07-18 20:36:39 +0500

    Второй коммит

--- novaiapapka/server.js
+++ novaiapapka/server.js
@@ -7,13 +7,8 @@ app.get('/main', (req,res)=>{
     // res.send('Halo World!');
     res.render('index');
 });
-
-app.get('/main3', (req,res)=>{
-    res.send('Poer World!');
-});
-
 app.get('/main2', (req,res)=>{
-    res.send('Bee World!');
+    res.render('index2')
 });
 
 
